f61ad9f241e86f785040669791d843ab704df54e
Oleh Yashchyshyn <oleh.yashchyshyn@gmail.com>
2023-01-04 17:05:15 +0200

    development
    Update Program.cs

--- TradeHero/Src/TradeHero.Runner/Program.cs
+++ TradeHero/Src/TradeHero.Runner/Program.cs
@@ -2,6 +2,7 @@
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using TradeHero.Contracts.Base.Enums;
 using TradeHero.Contracts.Services;
 using TradeHero.DependencyResolver;
 using TradeHero.Runner.Helpers;
@@ -42,29 +43,47 @@ internal static class Program
                 throw new Exception("There is an error during user creation. Please see logs.");
             }
             
-            var environment = host.Services.GetRequiredService<IEnvironmentService>();
+            var environmentService = host.Services.GetRequiredService<IEnvironmentService>();
             
             await host.StartAsync();
 
             await host.WaitForShutdownAsync();
 
-            if (environment.CustomArgs.ContainsKey("--urd=") && !string.IsNullOrWhiteSpace(environment.CustomArgs["--urd="])
-                && environment.CustomArgs.ContainsKey("--urn=") && !string.IsNullOrWhiteSpace(environment.CustomArgs["--urn="]))
+            if (environmentService.CustomArgs.ContainsKey("--urd=") && !string.IsNullOrWhiteSpace(environmentService.CustomArgs["--urd="])
+                && environmentService.CustomArgs.ContainsKey("--urn=") && !string.IsNullOrWhiteSpace(environmentService.CustomArgs["--urn="]))
             {
-                var baseFolderPath = environment.CustomArgs["--bfp="];
-                var updateFolderPath = environment.CustomArgs["--ufp="];
-                var mainApplicationName = environment.CustomArgs["--man="];
-                var downloadedApplicationName = environment.CustomArgs["--dan="];
+                var baseFolderPath = environmentService.CustomArgs["--bfp="];
+                var updateFolderPath = environmentService.CustomArgs["--ufp="];
+                var mainApplicationName = environmentService.CustomArgs["--man="];
+                var downloadedApplicationName = environmentService.CustomArgs["--dan="];
+                var workingDirectory = environmentService.CustomArgs["--urd="];
+                var fileName = environmentService.CustomArgs["--urn="];
 
-                var workingDirectory = environment.CustomArgs["--urd="];
-                var fileName = environment.CustomArgs["--urn="];
-                var arguments = $"--bfp={baseFolderPath} --ufp={updateFolderPath} --man={mainApplicationName} --dan={downloadedApplicationName}";
-
-                var process = new Process();
-                process.StartInfo.FileName = Path.Combine(workingDirectory, fileName);
-                process.StartInfo.Arguments = arguments;
-                process.StartInfo.UseShellExecute = true;
-                process.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+                var processStartInfo = new ProcessStartInfo();
+                switch (environmentService.GetCurrentOperationSystem())
+                {
+                    case OperationSystem.None:
+                        break;
+                    case OperationSystem.Linux:
+                        break;
+                    case OperationSystem.Osx:
+                        break;
+                    case OperationSystem.Windows:
+                        processStartInfo.FileName = "cmd.exe";
+                        processStartInfo.Arguments = $"/K {Path.Combine(workingDirectory, fileName)} " + 
+                            $"--bfp={baseFolderPath} --ufp={updateFolderPath} --man={mainApplicationName} --dan={downloadedApplicationName}";
+                        processStartInfo.UseShellExecute = false;
+                        processStartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+                        break;
+                    default:
+                        throw new ArgumentOutOfRangeException();
+                }
+                
+                var process = new Process
+                {
+                    StartInfo = processStartInfo
+                };
+                
                 process.Start();
             }
         }
