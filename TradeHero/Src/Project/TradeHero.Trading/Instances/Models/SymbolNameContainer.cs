5d7091724313114a2a262177471e275e6648c308
Oleh Yashchyshyn <oleh.yashchyshyn@gmail.com>
2023-01-16 08:41:51 +0200

    development
    Update SpotClusterVolumeInstance.cs
    Update SymbolNameContainer.cs

---
+++ TradeHero/Src/Project/TradeHero.Trading/Instances/Models/SymbolNameContainer.cs
@@ -0,0 +1,9 @@
+namespace TradeHero.Trading.Instances.Models;
+
+internal class SymbolNameContainer
+{
+    public string SpotName { get; init; } = string.Empty;
+    public string FuturesUsdName { get; init; } = string.Empty;
+    public string BaseFuturesUsdAsset { get; init; } = string.Empty;
+    public string QuoteAsset { get; init; } = string.Empty;
+}
