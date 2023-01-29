14c93370182b4fd8edbc2f4429b873d684d8cadd
Oleh Yashchyshyn <oleh.yashchyshyn@gmail.com>
2023-01-29 21:18:02 +0200

    TH-25
    Add MenuType.cs
    Update AppHostedService.cs
    Update ConsoleMenu.cs
    Update IMenuService.cs
    Update TelegramMenu.cs
    Update UpdateInfo.cs

--- TradeHero/Src/Project/TradeHero.Core/Types/Services/Models/Store/UpdateInfo.cs
+++ TradeHero/Src/Project/TradeHero.Core/Types/Services/Models/Store/UpdateInfo.cs
@@ -3,5 +3,4 @@ namespace TradeHero.Core.Types.Services.Models.Store;
 public class UpdateInfo
 {
     public bool IsNeedToUpdateApplication { get; set; }
-    public bool IsApplicationAfterUpdate { get; set; }
 }
